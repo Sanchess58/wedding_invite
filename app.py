2eb46e02c24664dd668951c7b29d2146a2e489ef
Александр Сотников <sanchess2015@icloud.com>
2023-05-31 00:10:44 +0300

    Добавлена страничка со всеми гостями, возможность удалять гостей

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 from flask_wtf import FlaskForm 
 from sqlalchemy_utils import PhoneNumberType
@@ -6,14 +6,14 @@ from wtforms import StringField
 import os
 from wtforms.validators import Length, DataRequired
 
-app = Flask(__name__)
+application = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///guest.db'
+application.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///guest.db'
 
-db = SQLAlchemy(app)
+db = SQLAlchemy(application)
 
 SECRET_KEY = os.urandom(32)
-app.config['SECRET_KEY'] = SECRET_KEY
+application.config['SECRET_KEY'] = SECRET_KEY
 
 class Guest(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -22,13 +22,13 @@ class Guest(db.Model):
     is_kid = db.Column(db.Boolean)
 
     def __repr__(self):
-        return f'<Guest {self.name}>'
+        return f'{self.name} | {self.number}'
     
 class GuestForm(FlaskForm):
     name = StringField(validators=[Length(max=150)], render_kw={"placeholder": "Введите ФИО"})
     phone = StringField(validators=[DataRequired()], render_kw={"placeholder": "Введите номер телефона"})
 
-@app.route('/', methods=['POST', 'GET'])
+@application.route('/', methods=['POST', 'GET'])
 def main():
     form = GuestForm() 
     if request.method == 'POST':
@@ -42,5 +42,22 @@ def main():
     return render_template('test2.html', form=form)
 
 
+@application.route("/get_guests", methods=['GET'])
+def get_guests():
+    guests = Guest.query.all()
+    return render_template('guests.html', guests=guests)
+
+@application.route("/guest/<int:id>/delete/")
+def delete_guest(id):
+    guest_del = Guest.query.get_or_404(id)
+    try:
+        db.session.delete(guest_del)
+        db.session.commit()
+        return redirect("/get_guests")
+    except:
+        return "ПРи удалении произошла ошибка!"
+
+
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    application.run(debug=True)
